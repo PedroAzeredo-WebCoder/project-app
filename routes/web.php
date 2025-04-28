82acfd2ad5884e1c7d6b3c24276d99fcacf3cf4f
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-04-27 23:46:06 -0300

    feat: implementation of the first routes

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,17 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
+Route::get('/contato', function () {
+    return 'Contato';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre Nós';
+});
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
