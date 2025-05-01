c3dca254aa839afbdfbb08fc9b37b39288a43f83
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-01 01:01:11 -0300

    feat: added verification using unless, checking for suppliers

--- resources/views/admin/fornecedor/index.blade.php
+++ resources/views/admin/fornecedor/index.blade.php
@@ -2,13 +2,17 @@
     {{-- This comment will not be present in the rendered HTML --}}
     <h1>Fornecedores</h1>
 
-    @foreach ($fornecedores as $fornecedor)
-        @if ($fornecedor['status'] == 'F')
-            <p style="color: red;">{{ $fornecedor['nome'] }}</p>
-        @elseif ($fornecedor['status'] == 'T')
-            <p style="color: green;">{{ $fornecedor['nome'] }}</p>
-        @else
-            <p>{{ $fornecedor['nome'] }}</p>
-        @endif  
-    @endforeach
+    @unless (count($fornecedores) > 0)
+        <p>Nenhum fornecedor encontrado</p>
+    @else
+        @foreach ($fornecedores as $fornecedor)
+            @if ($fornecedor['status'] == 'F')
+                <p style="color: red;">{{ $fornecedor['nome'] }}</p>
+            @elseif ($fornecedor['status'] == 'T')
+                <p style="color: green;">{{ $fornecedor['nome'] }}</p>
+            @else
+                <p>{{ $fornecedor['nome'] }}</p>
+            @endif  
+        @endforeach
+    @endunless
 </div>
