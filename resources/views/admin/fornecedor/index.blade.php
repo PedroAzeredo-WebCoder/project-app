11794c60a69cda39443db327d050ef02c4940abb
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-01 01:10:20 -0300

    feat: added @isset method to check for data existence in a given query

--- resources/views/admin/fornecedor/index.blade.php
+++ resources/views/admin/fornecedor/index.blade.php
@@ -2,17 +2,21 @@
     {{-- This comment will not be present in the rendered HTML --}}
     <h1>Fornecedores</h1>
 
-    @unless (count($fornecedores) > 0)
-        <p>Nenhum fornecedor encontrado</p>
+    @isset($fornecedores)
+        @unless (count($fornecedores) > 0)
+            <p>Nenhum fornecedor encontrado</p>
+        @else
+            @foreach ($fornecedores as $fornecedor)
+                @if ($fornecedor['status'] == 'F')
+                    <p style="color: red;">{{ $fornecedor['nome'] }}</p>
+                @elseif ($fornecedor['status'] == 'T')
+                    <p style="color: green;">{{ $fornecedor['nome'] }}</p>
+                @else
+                    <p>{{ $fornecedor['nome'] }}</p>
+                @endif  
+            @endforeach
+        @endunless
     @else
-        @foreach ($fornecedores as $fornecedor)
-            @if ($fornecedor['status'] == 'F')
-                <p style="color: red;">{{ $fornecedor['nome'] }}</p>
-            @elseif ($fornecedor['status'] == 'T')
-                <p style="color: green;">{{ $fornecedor['nome'] }}</p>
-            @else
-                <p>{{ $fornecedor['nome'] }}</p>
-            @endif  
-        @endforeach
-    @endunless
+        <p>Lista de fornecedores não disponível.</p>
+    @endisset
 </div>
