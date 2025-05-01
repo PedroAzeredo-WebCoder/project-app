a68a45de2637b31858343255b910f5d9dfe19af7
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-01 20:38:00 -0300

    refactor: changing the layout of data displayed on screen to table format

--- resources/views/admin/fornecedor/index.blade.php
+++ resources/views/admin/fornecedor/index.blade.php
@@ -6,94 +6,104 @@
         @unless (count($fornecedores) > 0)
             <p>Nenhum fornecedor encontrado</p>
         @else
-            @foreach ($fornecedores as $fornecedor)
-                Interação {{ $loop->iteration }}
-                @if ($loop->first)
-                    <p>Primeiro fornecedor</p>
-                @endif
-                @if ($loop->last)
-                    <p>Último fornecedor</p>
+            <table border="1" width="100%" cellpadding="6" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th>Status</th>
+                        <th>CNPJ</th>
+                        <th>DDD</th>
+                        <th>Categorias</th>
+                        <th>Atendimentos</th>
+                        <th>Horários</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($fornecedores as $fornecedor)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>
+                                @isset($fornecedor['status'])
+                                    @if ($fornecedor['status'] == 'F')
+                                        <span style="color: red;">{{ $fornecedor['nome'] }}</span>
+                                    @elseif ($fornecedor['status'] == 'T')
+                                        <span style="color: green;">{{ $fornecedor['nome'] }}</span>
+                                    @else
+                                        {{ $fornecedor['nome'] }}
+                                    @endif
+                                @else
+                                    {{ $fornecedor['nome'] ?? 'Não informado' }}
+                                @endisset
+                                @if ($loop->first)
+                                    <br><small>(Primeiro fornecedor)</small>
+                                @endif
+                                @if ($loop->last)
+                                    <br><small>(Último fornecedor)</small>
+                                    <br><small>Total: {{ $loop->count }}</small>
+                                @endif
+                            </td>
+                            <td>{{ $fornecedor['status'] ?? 'Não informado' }}</td>
+                            <td>
+                                @empty($fornecedor['cnpj'])
+                                    Não informado
+                                @else
+                                    {{ $fornecedor['cnpj'] }}
+                                @endempty
+                            </td>
+                            <td>
+                                @switch($fornecedor['ddd'])
+                                    @case('51')
+                                        Rio Grande do Sul
+                                    @break
 
-                    Total: {{ $loop->count }}
-                @endif
+                                    @case('11')
+                                        São Paulo
+                                    @break
 
-                @isset($fornecedor['status'])
-                    @if ($fornecedor['status'] == 'F')
-                        <p style="color: red;">{{ $fornecedor['nome'] }}</p>
-                    @elseif ($fornecedor['status'] == 'T')
-                        <p style="color: green;">{{ $fornecedor['nome'] }}</p>
-                    @else
-                        <p>{{ $fornecedor['nome'] }}</p>
-                    @endif
-                @endisset
-
-                @empty($fornecedor['cnpj'])
-                    <p>CNPJ: Não informado</p>
-                @else
-                    <p>CNPJ: {{ $fornecedor['cnpj'] }}</p>
-                @endempty
-
-                Status: {{ $fornecedor['status'] ?? 'Não informado' }}
-
-                @switch($fornecedor['ddd'])
-                    @case('51')
-                        <p>DDD: Rio Grande do Sul</p>
-                    @break
-
-                    @case('11')
-                        <p>DDD: São Paulo</p>
-                    @break
-
-                    @default
-                        <p>DDD: Não informado</p>
-                @endswitch
-
-                {{-- Exibe todas as categorias em uma linha separada por vírgula usando @for --}}
-                @if (!empty($fornecedor['categorias']) && is_array($fornecedor['categorias']))
-                    <p>Categorias:
-                        @for ($i = 0; $i < count($fornecedor['categorias']); $i++)
-                            {{ $fornecedor['categorias'][$i] }}
-                            @if ($i < count($fornecedor['categorias']) - 1)
-                                ,
-                            @endif
-                        @endfor
-                    </p>
-                @else
-                    <p>Categorias: Não informado</p>
-                @endif
-
-                @php
-                    $i = 0;
-                @endphp
-                @if (!empty($fornecedor['atendimento']) && is_array($fornecedor['atendimento']))
-                    <p>Atendimentos:
-                        @php $i = 0; @endphp
-                        @while ($i < count($fornecedor['atendimento']))
-                            {{ $fornecedor['atendimento'][$i] }}@if ($i < count($fornecedor['atendimento']) - 1)
-                                ,
-                            @endif
-                            @php $i++; @endphp
-                        @endwhile
-                    </p>
-                @else
-                    <p>Atendimentos: Não informado</p>
-                @endif
-
-                <p>Horários:
-                    @forelse ($fornecedor['horarios'] ?? [] as $index => $horario)
-                        {{ $horario }}
-                        @if (!$loop->last)
-                            ,
-                        @endif
-                    @empty
-                        Não informado
-                    @endforelse
-                </p>
-
-                <hr>
-            @endforeach
-        @endunless
-    @else
-        <p>Lista de fornecedores não disponível.</p>
-    @endisset
-</div>
+                                    @default
+                                        Não informado
+                                @endswitch
+                            </td>
+                            <td>
+                                @if (!empty($fornecedor['categorias']) && is_array($fornecedor['categorias']))
+                                    @for ($i = 0; $i < count($fornecedor['categorias']); $i++)
+                                        {{ $fornecedor['categorias'][$i] }}@if ($i < count($fornecedor['categorias']) - 1)
+                                            ,
+                                        @endif
+                                    @endfor
+                                @else
+                                    Não informado
+                                @endif
+                            </td>
+                            <td>
+                                @if (!empty($fornecedor['atendimento']) && is_array($fornecedor['atendimento']))
+                                    @php $i = 0; @endphp
+                                    @while ($i < count($fornecedor['atendimento']))
+                                        {{ $fornecedor['atendimento'][$i] }}@if ($i < count($fornecedor['atendimento']) - 1)
+                                            ,
+                                        @endif
+                                        @php $i++; @endphp
+                                    @endwhile
+                                @else
+                                    Não informado
+                                @endif
+                            </td>
+                            <td>
+                                @forelse ($fornecedor['horarios'] ?? [] as $index => $horario)
+                                    {{ $horario }}@if (!$loop->last)
+                                        ,
+                                    @endif
+                                    @empty
+                                        Não informado
+                                    @endforelse
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endunless
+        @else
+            <p>Lista de fornecedores não disponível.</p>
+        @endisset
+    </div>
