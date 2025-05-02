acdb549236adb40a08f798726a394fc7900515cf
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-02 01:11:37 -0300

    feat: added bootstrap style menu

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,11 +1,46 @@
-<div>
-    <h1>Contato</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-    <nav>
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('sobre-nos') }}">Sobre Nós</a></li>
-            <li><a href="{{ route('contato') }}">Contato</a></li>
-        </ul>
-    </nav>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contato</title>
+    <link rel="stylesheet" href="{{ asset('build/assets/css/app.B8p3F0Ej.css') }}">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('build/assets/img/webcoder.svg') }}" alt="Logo" width="100">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('sobre-nos') }}">Sobre Nós</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contato') }}">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+</body>
+
+<script src="{{ asset('build/assets/js/app.Dsd7o7FW.js') }}"></script>
+
+</html>
