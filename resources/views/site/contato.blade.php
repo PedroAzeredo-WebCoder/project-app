01c03bdd2ba4ec62d7604c9aafa2b8ac08a64bf3
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-03 15:19:19 -0300

    feat: created the first form via get and displaying data sent in the same controller

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,7 +1,49 @@
-@extends('site.components.layout')
+@extends('layouts.app')
 
 @section('content')
     <main>
-        <h1>Contato</h1>
+        <section>
+            <div class="container">
+                <div class="row justify-content-end">
+                    <div class="col-6">
+                        <h1>Contato</h1>
+                        <form class="row g-3" action="{{ route('contato') }}" method="GET">
+                            <div class="col-md-6">
+                                <label for="f_nome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" name="f_nome" id="f_nome">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="f_telefone" class="form-label">Telefone</label>
+                                <input type="tel" class="form-control" name="f_telefone" id="f_telefone">
+                            </div>
+                            <div class="col-12">
+                                <label for="f_email" class="form-label">E-mail</label>
+                                <input type="email" class="form-control" name="f_email" id="f_email">
+                            </div>
+                            <div class="col-12">
+                                <label for="f_estado" class="form-label">Estado</label>
+                                <select id="f_estado" class="form-select" name="f_estado">
+                                    <option selected>Selecione o estado</option>
+                                    <option value="AC">Acre</option>
+                                    <option value="AL">Alagoas</option>
+                                    <option value="AP">Amapá</option>
+                                    <option value="AM">Amazonas</option>
+                                    <option value="BA">Bahia</option>
+                                </select>
+                            </div>
+                            <div class="col-12">
+                                <label for="f_mensagem" class="form-label">Mensagem</label>
+                                <textarea class="form-control" name="f_mensagem" id="f_mensagem" rows="3"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
