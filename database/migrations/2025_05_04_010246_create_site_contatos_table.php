f501a01629c0bdfa42435b5bc2ba28cf859acd19
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-04 00:49:32 -0300

    feat: create new migration for contact form submissions

---
+++ database/migrations/2025_05_04_010246_create_site_contatos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('email', 150);
+            $table->string('telefone', 20);
+            $table->integer('estado_id');
+            $table->mediumText('mensagem'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site_contatos');
+    }
+};
