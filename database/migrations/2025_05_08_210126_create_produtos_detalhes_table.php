41cf217de2d8ce4cb90bf319245b2409fa9432c8
Pedro Azeredo <pedro.azeredo93@gmail.com>
2025-05-08 23:42:04 -0300

    feat: creating migration, using foreign key in constrained for table relationship

---
+++ database/migrations/2025_05_08_210126_create_produtos_detalhes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos_detalhes');
+    }
+};
